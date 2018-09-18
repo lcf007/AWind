8ad7092cfdae17de322405451ab72c08c1e53e1a
lcf007 <lcf007@msn.com>
2018-09-18 12:05:47 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+
+class App extends Component {
+
+  onMyBlog(){
+    window.location.href = 'http://www.awind.top/blog/'
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Stay Hungry. Stay Foolish.</h1>
+          <button className='button' onClick={this.onMyBlog.bind(this)}>My Blog</button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
